461752c0ac9f54024962ab5b84dbf04ee251e86a
baraaabuzeid <baraa@aleph-labs.com>
2022-03-05 20:43:13 +0200

    connect screens to main navigation stack

--- src/navigations/stacks/MainStack.tsx
+++ src/navigations/stacks/MainStack.tsx
@@ -2,7 +2,8 @@ import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Home,{navigationOptions} from "../../screens/Home";
-import {APP_SCREENS} from '../../screens/screens';
+import {SCREENS,Buy,Profile,Transactions,Notifications,Scan} from '../../screens'
+import TabBar from "../../components/TabBar";
 
 
 
@@ -10,11 +11,28 @@ import {APP_SCREENS} from '../../screens/screens';
 const Tab = createBottomTabNavigator();
 
 const MainStack = () => {
+  const {APP_SCREENS} = SCREENS
   return (
-    <Tab.Navigator initialRouteName={APP_SCREENS.HomeScreen}>
+    <Tab.Navigator initialRouteName={APP_SCREENS.HomeScreen}  tabBar={TabBar} >
         <Tab.Screen 
             component={Home} 
             name={APP_SCREENS.HomeScreen}
+            options={navigationOptions} />
+        <Tab.Screen 
+            component={Buy} 
+            name={APP_SCREENS.BuyScreen}
+            options={navigationOptions} />
+        <Tab.Screen 
+            component={Transactions} 
+            name={APP_SCREENS.TransactionsScreen}
+            options={navigationOptions} />
+        <Tab.Screen 
+            component={Profile} 
+            name={APP_SCREENS.ProfileScreen}
+            options={navigationOptions} />
+        <Tab.Screen 
+            component={Scan} 
+            name={APP_SCREENS.ScanScreen}
             options={navigationOptions} />
     </Tab.Navigator>
  
---
+++ src/screens/index.ts
@@ -0,0 +1,9 @@
+export { default as Home } from './Home';
+export { default as Buy } from './Buy'
+export { default as Notifications } from './Notifications';
+export { default as Profile } from './Profile';
+export { default as Transactions } from './Transactions';
+export { default as Scan } from './Scan';
+export * as  SCREENS from './screens'
+
+
