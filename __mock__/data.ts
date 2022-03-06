729525163991444e9ede18de42f0d440eeb148bc
baraaabuzeid <baraa@aleph-labs.com>
2022-03-06 17:51:45 +0200

    add mock data

--- __mock__/data.ts
+++ __mock__/data.ts
@@ -1,9 +1,9 @@
 export const transactionsList = [
-    {"id": "T100", "notification_read": false,amount:1000},
-    {"id": "T200", "notification_read": true,amount:4000},
-    {"id": "T300", "notification_read": false,amount:3000},
-    {"id": "T400", "notification_read": false,amount:5000},
-    {"id": "T500", "notification_read": false,amount:6000},
-    {"id": "T600", "notification_read": false,amount:5900},
-    {"id": "T700", "notification_read": true,amount:7500},
+    {"id": "T100", "notification_read": false},
+    {"id": "T200", "notification_read": true},
+    {"id": "T300", "notification_read": false},
+    {"id": "T400", "notification_read": false},
+    {"id": "T500", "notification_read": false},
+    {"id": "T600", "notification_read": false},
+    {"id": "T700", "notification_read": true},
 ]
