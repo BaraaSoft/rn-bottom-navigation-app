9939c2622d9591ef9bfc9d30c67f2d0b0200484e
baraaabuzeid <baraa@aleph-labs.com>
2022-03-07 20:55:44 +0200

    refactor profile screen

--- src/screens/main/Profile.tsx
+++ src/screens/main/Profile.tsx
@@ -1,15 +1,26 @@
 import React from "react";
-import {View,Text} from 'react-native';
+import {TouchableOpacity} from 'react-native';
+import { navigate } from "../../services/navigationService";
+import { APP_SCREENS } from "../screens";
+import { Container,Title ,Link} from "./Profile.style";
 
 
 
 
 const Profile = ()=>{
+    const onPress = ()=>{
+        navigate("NotificationStack",APP_SCREENS.NotificationsScreen)
+    }
     return(
-        <View>
-            <Text>Profile screen</Text>
-        </View>
+        <Container>
+            <Title>Profile screen</Title>
+            <TouchableOpacity accessibilityLabel="Goto notifications button" onPress={onPress} >
+                <Link>Go to Notifications</Link>
+            </TouchableOpacity>
+        </Container>
     )
 }
 
 export default Profile
+
+
