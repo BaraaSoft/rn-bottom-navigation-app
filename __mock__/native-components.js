f5caedb29fc6bd97f92547a38ee8f59a95ea86bb
baraaabuzeid <baraa@aleph-labs.com>
2022-03-08 00:00:03 +0200

    update mock tests components

--- __mock__/native-components.js
+++ __mock__/native-components.js
@@ -96,4 +96,15 @@ jest.mock("react-native/Libraries/Animated/NativeAnimatedHelper");
 
 jest.mock("react-native/Libraries/EventEmitter/NativeEventEmitter");
 
+jest.mock("@react-navigation/native", () => {
+  const actualNav = jest.requireActual("@react-navigation/native");
+  return {
+    ...actualNav,
+    useNavigation: () => ({
+      navigate: jest.fn(),
+      dispatch: mockedDispatch,
+    }),
+  };
+});
+
 StatusBar.currentHeight = 20;
